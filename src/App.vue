2e3bb5a00e8edd985dc0505218f2b0b269fdcb4c
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2021-12-11 09:20:39 -0300

    update home page to be empty

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,14 @@
           width="100"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <!-- <v-img
+             alt="Vuetify Name"
+             class="shrink mt-1 hidden-sm-and-down"
+             contain
+             min-width="100"
+             src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+             width="100"
+             /> -->
       </div>
 
       <v-spacer></v-spacer>
